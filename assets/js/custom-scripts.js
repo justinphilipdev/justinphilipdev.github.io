d0a80279549f2235a5bc71a546eedf2f6fa1ee89
Justin Philip <63745563+justinphilipdev@users.noreply.github.com>
2020-05-15 14:24:29 +0530

    Disable JS form submit

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -337,8 +337,8 @@
             submitMSG(false, "Did you fill in the form properly?");
           } else {
             // everything looks good!
-            event.preventDefault();
-            submitForm();
+            //event.preventDefault();
+            //submitForm();
           }
        });
     
